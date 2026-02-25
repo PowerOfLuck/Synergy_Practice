0c03b1cc815707d50cf988ecb82c9d1c7ec7dc87
Anthony <136853470+PowerOfLuck@users.noreply.github.com>
2026-02-25 21:11:07 +0500

    Update case_1.py

--- case_1/case_1.py
+++ case_1/case_1.py
@@ -1,39 +1,42 @@
 import numpy as np
 
-list_numbers = np.array([3,-6,4,-2,2,-5,-3,-9,1,-2])
-
+list_numbers = np.array([3, -6, 4, -2, 2, -5, -3, -9, 1, -2])
 
 def num_sum(numbers):
+    if len(numbers) == 0:
+        return 0
+
     result = 0
 
     # e[0] - максимальный элемент
     # e[1] - минимальный элемент
-    # e[2] - id максимального элемента
-    # e[3] - id минимального элемента
-    e = [0,0,0,0]
+    # e[2] - индекс минимального элемента
+    # e[3] - индекс максимального элемента
+    e = [numbers[0], numbers[0], 0, 0]
 
-    # мин элемент и его id
+    # минимальный элемент и его индекс
     for i in range(len(numbers)):
         if numbers[i] < e[1]:
             e[1] = numbers[i]
             e[2] = i
 
-    # макс элемент и его id
+    # максимальный элемент и его индекс
     for x in range(len(numbers)-1, -1, -1):
         if numbers[x] > e[0]:
             e[0] = numbers[x]
             e[3] = x
 
+    # для удобства упорядочиваем индексы
     if e[2] > e[3]:
-        # для удобства при следующей итерации
-        e[2],e[3] = e[3],e[2]
+        e[2], e[3] = e[3], e[2]
 
-    # счет отрицательных чисел в промежутке
-    for u in range(e[2], e[3]):
+    # суммируем отрицательные элементы между индексами
+    for u in range(e[2] + 1, e[3]):
         if numbers[u] < 0:
             result += numbers[u]
+
     return result
 
 result = num_sum(list_numbers)
-
 print(result)
+
